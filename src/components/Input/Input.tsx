dbb300deee4c5485b5de0ac5173245032cbba42d
let-robots-reign <zotov.alexei.1@yandex.ru>
2021-08-31 10:48:39 +0300

    scss styles, Input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export type InputProps = {
+    value: string
+    placeholder: string
+    onChange?: (e: React.ChangeEvent) => void
+};
+
+const Input: React.FC<InputProps> = ({ value, placeholder, onChange }) => {
+    return (
+        <input
+            type="text"
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+        />
+    );
+};
+
+export default Input;
