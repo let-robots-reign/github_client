dc2763d7ce866aeb11f2dab2160aa6b651f9860a
let-robots-reign <zotov.alexei.1@yandex.ru>
2021-09-01 17:16:09 +0300

    create ReposSearchList

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import '@styles/App.scss';
+import '@styles/theme.scss';
 
 import './root/root';
 
-import logo from './logo.svg';
+import ReposSearchList from './pages/ReposSearchList';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <ReposSearchList></ReposSearchList>
         </div>
     );
 }
---
+++ src/components/RepoTile/RepoTile.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import Avatar from '@components/Avatar';
+import StarIcon from '@components/StarIcon';
+import orangePic from '@img/orange.jpg';
+
+import classes from './RepoTile.module.scss';
+
+export type RepoItem = {
+    title: string;
+    orgName: string;
+    avatarSrc?: string;
+    stars: number;
+    updatedAt: string;
+};
+
+export type RepoItemProps = {
+    repoItem: RepoItem;
+    onClick?: (e: React.MouseEvent) => void;
+};
+
+const RepoTile: React.FC<RepoItemProps> = ({ repoItem, onClick }) => {
+    return (
+        <div className={classes['git-repo-tile']} onClick={onClick}>
+            <Avatar src={orangePic} alt={repoItem.title}></Avatar>
+            <div className={classes['git-repo-tile__info-block']}>
+                <h3 className={classes['git-repo-tile__title']}>
+                    <a
+                        href="/"
+                        className={classes['git-repo-tile__title-link']}
+                    >
+                        {repoItem.title}
+                    </a>
+                </h3>
+                <a href="/" className={classes['git-repo-tile__org-link']}>
+                    {repoItem.orgName}
+                </a>
+                <div className={classes['git-repo-tile__bottom-info']}>
+                    <div className={classes['git-repo-tile__stars']}>
+                        <StarIcon fillColor="yellow"></StarIcon>
+                        <span className={classes['git-repo-tile__stars-count']}>
+                            {repoItem.stars}
+                        </span>
+                    </div>
+                    <div className={classes['git-repo-tile__updated']}>
+                        Updated at {repoItem.updatedAt}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RepoTile;
