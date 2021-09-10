859f6a8a74741d96649d390580931be78f05dec4
let-robots-reign <zotov.alexei.1@yandex.ru>
2021-09-10 20:21:23 +0300

    added BrowserRouter, Route, and Redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import '@styles/theme.scss';
 import 'antd/dist/antd.css';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+
 import ReposSearchPage from './pages/ReposSearchPage';
 
 function App() {
     return (
         <div className="App">
-            <ReposSearchPage />
+            <BrowserRouter>
+                <Route exact path="/repos" component={ReposSearchPage} />
+                <Redirect to="/repos" />
+            </BrowserRouter>
         </div>
     );
 }
